d71ff36e01db194874aeb7ce6e7719818c471fa4
Souren Hazra <sourenh94@gmail.com>
2023-08-21 20:35:58 +0530

    add

---
+++ app2.py
@@ -0,0 +1,124 @@
+import time
+
+import dash
+import dash_bootstrap_components as dbc
+import numpy as np
+import plotly.graph_objs as go
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = dbc.Container(
+    [
+        dcc.Store(id="store"),
+        html.H1("Sales Data Analysis and Visualization"),
+        html.Hr(),
+        dbc.Tabs(
+            [
+                dbc.Tab(label="Upload Data", tab_id="csv_file_upload"),
+                dbc.Tab(label="Trend Analysis", tab_id="trends"),
+                dbc.Tab(label="Visualizations", tab_id="visual"),
+            ],
+            id="tabs",
+            active_tab="csv_file_upload",
+        ),
+        html.Div(id="tab-content", className="p-4"),
+    ]
+)
+
+
+@app.callback(
+    Output("tab-content", "children"),
+    [Input("tabs", "active_tab"), Input("store", "data")],
+)
+def render_tab_content(active_tab, data):
+    if active_tab and data is not None:
+        if active_tab == "csv_file_upload":
+            return html.Div([
+                dcc.Upload(
+                    id='csv_file_upload',
+                    children=html.Div([
+                        'Drag and Drop or ',
+                        html.A('Select Files')
+                    ]),
+                    style={
+                        'width': '100%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '10px'
+                    },
+                    # Allow multiple files to be uploaded
+                    multiple=True
+                ),
+                html.Div(id='output-data-upload'),
+            ])
+        elif active_tab == "histogram":
+            return dbc.Row(
+                [
+                    dbc.Col(dcc.Graph(figure=data["hist_1"]), width=6),
+                    dbc.Col(dcc.Graph(figure=data["hist_2"]), width=6),
+                ]
+            )
+    return "No tab selected"
+
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('latin1')))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        html.H5(filename.replace(".csv","")+" Data set"),
+        dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+        html.Hr(),  # horizontal line
+    ])
+
+
+@callback(Output('output-data-upload', 'children'),
+          Input('csv_file_upload', 'contents'),
+          State('csv_file_upload', 'filename'),)
+def update_output(list_of_contents, list_of_names):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n) for c, n in
+           zip(list_of_contents, list_of_names)]
+        return children
+
+@app.callback(Output("store", "data"), [Input("button", "n_clicks")])
+def generate_graphs(n):
+    """
+    This callback generates three simple graphs from random data.
+    """
+    if not n:
+        # generate empty graphs when app loads
+        return {k: go.Figure(data=[]) for k in ["scatter", "hist_1", "hist_2"]}
+
+    # simulate expensive graph generation process
+    time.sleep(2)
+
+    # generate 100 multivariate normal samples
+    data = np.random.multivariate_normal([0, 0], [[1, 0.5], [0.5, 1]], 100)
+
+    scatter = go.Figure(
+        data=[go.Scatter(x=data[:, 0], y=data[:, 1], mode="markers")]
+    )
+    hist_1 = go.Figure(data=[go.Histogram(x=data[:, 0])])
+    hist_2 = go.Figure(data=[go.Histogram(x=data[:, 1])])
+
+    # save figures in a dictionary for sending to the dcc.Store
+    return {"scatter": scatter, "hist_1": hist_1, "hist_2": hist_2}
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8050)
